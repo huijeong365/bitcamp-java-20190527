a4cd69ff4f51d76e1488dc6275d66b172db01257
huijeong <huijung87@naver.com>
2019-07-04 17:58:43 +0900

    버전 별 작업내용 추가

--- bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
+++ bitcamp-java-application/src/main/java/com/eomcs/lms/App3.java
@@ -1,11 +1,30 @@
 package com.eomcs.lms;
 
+import java.util.Scanner;
+
 public class App3 {
   public static void main(String[] args) {
-    System.out.println("번호: 1");
-    System.out.println("내용: 게시글입니다.");
-    System.out.println("작성일: 2019-01-01");
-    System.out.println("조회수: 0");
+    java.io.InputStream keyboard = System.in;
+    Scanner keyScan = new Scanner(keyboard);
+    
+    System.out.print("번호?");
+    String no = keyScan.nextLine();
+    
+    System.out.print("내용?");
+    String memo = keyScan.nextLine();
+    
+    System.out.print("작성일?");
+    String makeDate = keyScan.nextLine();
+    
+    System.out.print("조회수?");
+    String fineTimes = keyScan.nextLine();
+    
+    System.out.println();
+    
+    System.out.println("번호: " + no);
+    System.out.println("내용: " + memo);
+    System.out.println("작성일: " + makeDate);
+    System.out.println("조회수: " + fineTimes);
   }
 
 }
