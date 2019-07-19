06ae233a3e90c10e0d5ec3c05c1191a85da7930b
huijeong <huijung87@naver.com>
2019-07-19 12:18:03 +0900

    bitcapm-java-application v23 완료

--- bitcamp-java-basic/src/main/java/algorithm/data_structure/queue/step3/LinkedList.java
+++ bitcamp-java-basic/src/main/java/algorithm/data_structure/queue/step3/LinkedList.java
@@ -1,9 +1,9 @@
 // LinkedList : 목록으로 다루는 값을 특정 타입으로 제한하기 위해 제네릭(generic) 적용하기
-package com.eomcs.util;
+package algorithm.data_structure.queue.step3;
 
 import java.lang.reflect.Array;
 
-public class LinkedList<T> implements List<T>{
+public class LinkedList<T> {
   Node<T> head;
   Node<T> tail;
   int size = 0;
@@ -13,7 +13,6 @@ public class LinkedList<T> implements List<T>{
 //    tail = head; // head의 값을 tail에 저장하라.
   }
   
-  @Override
   public boolean add(T value) {
     Node<T> temp = new Node<T>(value);
     if (head == null)
@@ -30,7 +29,6 @@ public class LinkedList<T> implements List<T>{
     return true;
   }
   
-  @Override
   public T get(int index) {
     if (index < 0 || index >= size)
       throw new IndexOutOfBoundsException("인덱스가 유효하지 않습니다!");
@@ -43,7 +41,6 @@ public class LinkedList<T> implements List<T>{
   }
   
   // 특정 위치의 값을 바꾼다.
-  @Override
   public T set(int index, T value) {
     if (index < 0 || index >= size)
       throw new IndexOutOfBoundsException("인덱스가 유효하지 않습니다!");
@@ -57,7 +54,6 @@ public class LinkedList<T> implements List<T>{
     return oldVal; // 변경 전 값을 리턴
   }
   
-  @Override
   public T remove(int index) {
     if (index < 0 || index >= size)
       throw new IndexOutOfBoundsException("인덱스가 유효하지 않습니다!");
@@ -91,12 +87,10 @@ public class LinkedList<T> implements List<T>{
     return oldVal;
   }
   
-  @Override
   public int size() {
     return size;
   }
   
-  @Override
   public void clear() {
     if (size == 0)
       return;
@@ -112,7 +106,6 @@ public class LinkedList<T> implements List<T>{
     size = 0;
   }
 
-  @Override
   public Object[] toArray() {
     // LinkedList에 있는 데이터를 저장할 배열을 준비한다.
     Object[] arr = new Object[size];
@@ -140,7 +133,6 @@ public class LinkedList<T> implements List<T>{
     return arr;
   }
   
-  @Override
   @SuppressWarnings("unchecked")
   public T[] toArray(T[] a) {
     if (a.length < size) {
