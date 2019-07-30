d23bbc6d60ca8dba6985a3bb0c5a4c25de4e05dc
huijeong <huijung87@naver.com>
2019-07-30 17:30:01 +0900

    파일 입출력 예제

--- bitcamp-java-basic/src/main/java/ch21/g/Test02_1.java
+++ bitcamp-java-basic/src/main/java/ch21/g/Test02_1.java
@@ -1,14 +1,17 @@
-// 예외 처리 연습 II - 호출자에게 전달하기
+// 예외 처리 연습 II - 예외처리 안했을 때
 package ch21.g;
 
 public class Test02_1 {
 
-  public static void main(String[] args) throws RuntimeException { 
+  public static void main(String[] args) {
     
-      System.out.println(100 / 0);
-      // RuntimeException 예외가 발생되는 코드를 작성할 경우
-      // 메서드 선언부에 굳이 throws 를 포함할 필요는 없다.
-      // 그런 불편함을 없애기 위해 만든 예외가 RuntimeException이 아닌가!
+    System.out.println(100 / 0);
+    
+    // RuntimeException 에 대해서는 컴파일러가 예외처리를 요구하지 않는다.
+    // 다만 예외가 발생하면 호출자에게 전달한다.
+    // 여기서 예외가 발생하면 main()의 호출자인 JVM에게 전달한다.
+  
+    System.out.println("종료!");
   }
 
 }
